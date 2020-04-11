cc0d5e0b2b5507ec2ff2a03f02f7cfc6fe3401d3
Juliann Stainbrook <=>
2020-04-11 17:58:01 -0400

    completed styling on sign in - break before firebase

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 function App() {
   return (
+    <div>
     <Router>
     <Header />
        <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
        </Switch>
     </Router>
+    </div>
   );
 }
 
